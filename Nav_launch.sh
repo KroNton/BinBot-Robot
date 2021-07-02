31a11887d445d56d5c2d678e162f51622ad64fb8
KroNton <kyrlosfekry@gmail.com>
2021-07-02 20:26:37 +0200

    navigation DWA param

--- Nav_launch.sh
+++ Nav_launch.sh
@@ -5,4 +5,8 @@ xterm  -e  " roslaunch freenect_launch freenect.launch " &
 sleep 5
 xterm  -e  " roslaunch binbot_bringup robot_model.launch " &
 sleep 5
-xterm  -e  " roslaunch binbot_navigation amcl_demo_real.launch " 
+xterm  -e  " roslaunch binbot_navigation amcl_demo_real.launch " &
+sleep 5
+xterm  -e  " roslaunch binbot_navigation view_navigation.launch " 
+
+
--- SLAM_launch.sh
+++ SLAM_launch.sh
@@ -5,4 +5,7 @@ xterm  -e  " roslaunch freenect_launch freenect.launch " &
 sleep 5
 xterm  -e  " roslaunch binbot_bringup robot_model.launch " &
 sleep 5
-xterm  -e  " roslaunch differential_drive_arduino gmapping.launch " 
+xterm  -e  " roslaunch differential_drive_arduino gmapping.launch " &
+sleep 5
+xterm  -e  " roslaunch binbot_navigation view_mapping.launch " 
+
