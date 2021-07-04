88fdf774b6c8bda86898716fff5ed2f6a0b08bfc
KroNton <kyrlosfekry@gmail.com>
2021-07-04 11:23:08 +0200

    update gmapping parms

--- SLAM_launch.sh
+++ SLAM_launch.sh
@@ -5,7 +5,7 @@ xterm  -e  " roslaunch freenect_launch freenect.launch " &
 sleep 5
 xterm  -e  " roslaunch binbot_bringup robot_model.launch " &
 sleep 5
-xterm  -e  " roslaunch differential_drive_arduino gmapping.launch " &
+xterm  -e  " roslaunch binbot_navigation gmapping_demo.launch " &
 sleep 5
 xterm  -e  " roslaunch binbot_navigation view_mapping.launch " 
 
